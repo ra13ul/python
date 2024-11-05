465b23c6ff9b0550f0f5d773aa1d34cdf0719eaf
trecedename <trecedenueve@gmail.com>
2024-11-05 19:57:45 +0100

    Importando modulos propios

--- 05-calcularImpuesto.py
+++ 05-calcularImpuesto.py
@@ -1,17 +1,19 @@
 import funcionesGlobales
+#from funcionesGlobales import guion
 def total(*args):
-    pagoTotal=pago+pago*(impuesto/100)
-    return pagoTotal
+    precioTotal=precio+precio*(impuesto/100)
+    return precioTotal
 
 impuesto=21
-pago=500
-print(total(impuesto, pago))
+precio=500
+print('Precio con IVA ',total(impuesto, precio))
 
+#print(guion())
 print(funcionesGlobales.guion())
 
 def precioSinIva(*args):
-    totalIva=(impuesto*pago/100)
-    precioSinIva=pago-totalIva
-    datos="TOTAL PAGADO: {}€, IMPUESTO: {}€, SIN IVA: {}€".format(pago,totalIva, precioSinIva)
+    totalIva=(impuesto*precio/100)
+    precioSinIva=precio-totalIva
+    datos="TOTAL PAGADO: {}€, IMPUESTO: {}€, SIN IVA: {}€".format(precio,totalIva, precioSinIva)
     return datos
-print(precioSinIva(impuesto, pago))
+print(precioSinIva(impuesto, precio))
