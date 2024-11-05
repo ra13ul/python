c0ef9a04fea7d7b166836dc268373db7f51d723e
trecedename <trecedenueve@gmail.com>
2024-11-05 17:02:57 +0100

    añadiendo ...

--- 03-condicionales.py
+++ 03-condicionales.py
@@ -3,3 +3,13 @@ b=3
 if a < b:
     print('mayor')
     
+lista=list()
+
+#texto=input('Introduce texto:')
+texto='primero'
+if texto != 0:
+    lista.append(texto)
+    texto=input('Introduce texto:')
+else:
+    print('Has añadido textos')
+print(lista)
--- 04-lecturaDeArchivos.py
+++ 04-lecturaDeArchivos.py
@@ -1,10 +1,15 @@
+import pickle
+import csv
+from pathlib import Path
 #LEER ARCHIVOS
-pkl_file = open('2024-10-29_crypto.csv', 'rb')
+pkl_file = open('myfile.pkl', 'rb')
 diccionario = pickle.load(pkl_file)
+print(pkl_file)
 pkl_file.close()
+
 
 #escribir diccionario
 diccionario2 = {'a':1, 'b':2, 'c':3}
-salida = open('diccionario.pkl', 'wb')
-pickle.dump(diccionario2, salida)
+salida = open('myfile.pkl', 'wb')
+pickle.dump(diccionario, salida)
 salida.close()
