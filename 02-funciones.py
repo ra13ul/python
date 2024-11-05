a1f7054f7dbc362f79a2d724bd411effe6db9a4d
trecedename <trecedenueve@gmail.com>
2024-11-05 10:39:51 +0100

    otro casmbio

--- 02-funciones.py
+++ 02-funciones.py
@@ -20,3 +20,16 @@ print('\n')
 print("Suma de las variables a y b pasando valores\
       nuevos de a y b como argumentos")
 print(sumar(3,25))
+
+print('\n')
+print('Guardamos el resultado en una variable')
+def resta(c, d):
+    return c - d
+
+resultado=resta(5,3)
+print(resultado)
+
+c = resultado
+d= 10
+
+print(resta(c, d))
