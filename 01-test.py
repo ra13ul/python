f8a95b4c56880a3a1ec2c5e1c7c731ee1c186e8c
trecedename <trecedenueve@gmail.com>
2024-11-05 10:18:26 +0100

    nuevo archivo, practicas con funciones

--- 01-test.py
+++ 01-test.py
@@ -1,3 +1,43 @@
+# mac adress  0E:80:62:21:7A:D2, 192.168.1.133
+
+import math
+import random
+import pandas as pd
 print('hello wordl')
 nombre="raul"
 print(nombre)
+
+#Comentario de una linea
+
+"""
+comentario de
+varias lineas
+tres comillas simples o dobles
+"""
+
+a=5
+b=6
+
+imprime=True
+
+if imprime:
+    print(a*b)
+
+#usando math y random
+import math
+print(math.pi)
+
+print(math.sqrt(9))
+
+
+aleatorio=random.random ()
+print('numero aleatorio:',aleatorio)
+
+lista = [1, 2, 3, 4] 
+#print(random.choice(lista))
+
+random.shuffle(lista)
+print(lista)
+
+data = pd.DataFrame(lista)
+print(data)
---
+++ pandas.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import csv
+
+data=pd.read_csv('./2024-10-29_crypto.csv')
+data()
