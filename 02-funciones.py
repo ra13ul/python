f2362167db59bf6cef2e7c2ca300010cfbd9280e
trecedename <trecedenueve@gmail.com>
2024-11-05 10:32:27 +0100

    modificando el archivo de funciones

--- 02-funciones.py
+++ 02-funciones.py
@@ -1,1 +1,22 @@
 # FUNCIONES EN PYTHON
+def unaFuncion():
+    print('dentro de la funcion')
+
+unaFuncion()
+
+a=5
+b=3
+
+def sumar(a,b):
+    return a+b
+
+print("Suma de las variables a y b")
+print(sumar(a,b))
+
+print("Suma de las variables a y b y suma 100 a lo que devuelve")
+print(sumar(a,b)+100)
+print('\n')
+
+print("Suma de las variables a y b pasando valores\
+      nuevos de a y b como argumentos")
+print(sumar(3,25))
