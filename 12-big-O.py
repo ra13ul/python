c466ceae4c04cae9a9ac059c9b6454a86012f18e
trecedename <trecedenueve@gmail.com>
2024-11-14 12:49:14 +0100

    V1.3

---
+++ 12-big-O.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x = \
+list(np.random.randint(low=1,high=500000,
+                       size=99999999))
+
+
+def constante(x:list) -> list: 
+    return x 
+constante(x) 
--- pandas.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import csv
-
-
-data=pd.read_csv('./2024-10-29_crypto.csv')
-data()
